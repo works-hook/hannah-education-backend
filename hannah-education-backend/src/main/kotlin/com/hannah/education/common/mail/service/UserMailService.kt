598763bf8d86b098c9ac3bb5571338b0c36cb438
Hannah <ghdcoalss33@gmail.com>
2022-12-16 13:30:03 +0900

    feat(redis): 인증 끝나고 레디스에 남아있는 값 삭제

--- hannah-education-backend/src/main/kotlin/com/hannah/education/common/mail/service/UserMailService.kt
+++ hannah-education-backend/src/main/kotlin/com/hannah/education/common/mail/service/UserMailService.kt
@@ -23,7 +23,8 @@ class UserMailService(
 
     fun checkCertification(certificationNumber: String) {
         redisUtils.getData(certificationNumber)
-            ?: throw MailSendException("인증번호가 잘못되었거나 인증 시간이 초과되었습니다. 다시 확인해주세요.");
+            ?: throw MailSendException("인증번호가 잘못되었거나 인증 시간이 초과되었습니다. 다시 확인해주세요.")
+        redisUtils.deleteData(certificationNumber)
     }
 
     private fun getMailMessage(email: String, certificationNumber: String): MimeMessage {
